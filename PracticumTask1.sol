fcb6aa8a7568cc7e687e8f3d99a6aa3f301a0159
slnkrty <45341645+slnkrty@users.noreply.github.com>
2022-09-20 15:57:43 +0300

    Update PracticumTask1.sol

--- PracticumTask1.sol
+++ PracticumTask1.sol
@@ -3,38 +3,38 @@ import "hardhat/console.sol";
 // SPDX-License-Identifier: MIT
 
 contract Task1{
-    struct Cuzdan
+    struct Wallet
     {
         address payable owner; 
-        uint bakiye;
+        uint balance;
     
     //owner = payable(msg.sender);
 
 }
-    Cuzdan cuzdan; //Cuzdan tipinde bir object olusturuldu
+    Wallet wallet; //Wallet tipinde bir object olusturuldu
   
-    function setCuzdan() public {
-        cuzdan.owner = payable(msg.sender);
+    function setWallet() public {
+        wallet.owner = payable(msg.sender);
    }
     function getOwner() public pure returns (address _owner) {    
         return _owner;
     }
-    function getBakiye() public view returns (uint) {
-      return cuzdan.bakiye; }
+    function getBalance() public view returns (uint) {
+      return wallet.balance; }
 
-    function paraGonder(uint _miktar) payable external{
-        cuzdan.bakiye += _miktar;
+    function sendMoney(uint _amount) payable external{
+        wallet.balance += _amount;
     }
-    function paraCek(uint _miktar, address payable destAddr) payable external{
+    function withdrawMoney(uint _amount, address payable destAddr) payable external{
         require(msg.sender == destAddr, "Hesap sahibi para cekebilir!"); //eger msg.sender(hesap sahibinin adresi) para cekilecek olan adresle aynı degilse uyarı mesajı yazdırılır
-        require(_miktar <= cuzdan.bakiye, "Yetersiz bakiye"); //cekilmek istenen miktar bakiyeden yüksek oldugunda uyarı mesajı yazılır
+        require(_amount <= wallet.balance, "Yetersiz bakiye"); //cekilmek istenen miktar bakiyeden yüksek oldugunda uyarı mesajı yazılır
             
-        cuzdan.bakiye -= _miktar;
-       // destAddr.transfer(_miktar); // send funds to given address
+        wallet.balance -= _amount;
+       // destAddr.transfer(_amount); // send funds to given address
     }
 
-    function bakiyeGoster() public view returns (string memory mesaj, uint _bakiye){
-        return ("Bakiye: ",cuzdan.bakiye);
+    function displayBalance() public view returns (string memory message, uint _balance){
+        return ("Bakiye: ",wallet.balance);
             
     }
     
